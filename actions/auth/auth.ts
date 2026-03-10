aed3565155f9e7b9bb416f4ad9127e77b9eb0e43
Marco Antonio Uscamayta S. <maus279000@gmail.com>
2026-03-09 20:22:42 -0900

    v1

---
+++ actions/auth/auth.ts
@@ -0,0 +1,58 @@
+'use server'
+
+import { createClient } from '@/lib/supabase/server'
+
+export async function login(formData: {
+    email: string,
+    password: string,
+}) {
+    const supabase = await createClient()
+
+    const { error, data } = await supabase.auth.signInWithPassword(formData)
+
+    if (error) {
+        return {
+            success: false,
+            message: error.message
+        }
+    }
+
+    return {
+        success: true,
+        message: "Usuario autenticado exitosamente",
+        data
+    }
+}
+
+export async function signup(formData: {
+    name: string
+    email: string,
+    password: string,
+}) {
+
+    const supabase = await createClient()
+
+    const { error, data } = await supabase.auth.signUp({
+
+        email: formData.email,
+        password: formData.password,
+        options: {
+            data: {
+                name: formData.name
+            }
+        }
+    })
+
+    if (error) {
+        return {
+            success: false,
+            message: error.message
+        }
+    }
+
+    return {
+        success: true,
+        message: "Usuario registrado exitosamente",
+        data
+    }
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import AuthForm from "@/components/auth/AuthForm";
+
+export default function Home() {
+  return (
+    <div className="padding:12">
+      <AuthForm type="sign-in" />
+    </div>
+  );
+}
